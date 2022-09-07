7e48dde58536a32ec51b0a75b7721f9370621cee
Joel Luna <>
2022-09-07 00:14:11 +0000

    I printed out the values of each operation and calculated the currency exchange from Euro to dollars.

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,20 @@
+print(10 * 5)
+print(10 ** 2)
+print(15 / 10)
+print(15 // 10)
+print(-15 // 10)
+print(15 % 10)
+print(10 % 15)
+print(10 % 10)
+print(0 % 10)
+print(10 / 15)
+
+rate = input("What is the currect exchange rate for the Euro to Dollar?")
+
+amount = input("amount of currency to exchange?")
+
+total = float(rate * amount)
+
+result = float(total - 3.0)
+
+print(result)
