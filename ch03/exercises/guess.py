b45382f7488b41674b399be036faf013b1af2363
Joel Luna <112986791+JoelLuna10@users.noreply.github.com>
2022-09-24 03:46:36 +0000

    Chapter 3 guess.py

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,19 +1,22 @@
 import random
+from random import randint
 
-num_guesses = 1
+num_guesses = 0
+guess_count = 2
+radnum = int(randint(1,10))
+guess_num = int(input("guess a number: "))
 
-correct_guess = false
-
-for i in range(5):
-  if not correct_guess:
-    guess_num = int(input("Please enter a quess:"))
-  num_guesses += 1
-  if guess_num > num:
-    print("your guess is too high")
-  elif guess_name < num:
-    print("your guess is too low")
+for i in range(guess_count):
+  if num_guesses == 3:
+      break
+  if guess_num != radnum:
+    guess_num = int(input("guess a number: "))
+    num_guesses += 1
+    if guess_num > radnum:
+      print("your guess is too high")
+    elif guess_num < radnum:
+      print("your guess is too low")
   else:
     print("Correct!")
+    print("It took you", num_guesses, "to get it right.")
     break
-
-print("It took you", num_quesses, "to get it right.")
