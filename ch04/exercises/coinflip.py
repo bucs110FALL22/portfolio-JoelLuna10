cab5580911c11275a90c2d4d27ff117b8eb61d57
Joel Luna <112986791+JoelLuna10@users.noreply.github.com>
2022-10-10 21:11:16 +0000

    ch4 coinflip complete

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,12 +1,33 @@
-import pygame
+import random
+import turtle
 
-for event in pygame.event.get():
-  if event.type == pygame.KEYDOWN:
-    if event.key == pygame.K_w:
-        print("Move the character forwards")
-    elif event.key == pygame.K_s:
-        print("Move the character backwards")
-    elif event.key == pygame.K_a:
-        print("Move the character left")
-    elif event.key == pygame.K_d:
-        print("Move the character right")
+koopa = turtle.Turtle()
+wn = turtle.Screen()
+koopa.shape("turtle")
+koopa.speed(4)
+
+distance = 10
+angle = 90
+is_in_screen = True
+wn = turtle.Screen() 
+
+while is_in_screen:
+  coin = random.randrange(0,2)
+  if coin == 0:
+    koopa.left(angle)
+  else:
+    koopa.right(angle)
+    
+  koopa.forward(distance)
+  turtleX = koopa.xcor()
+  turtleY = koopa.ycor()
+  x_range = wn.window_width()/2
+  y_range = wn.window_height()/2
+  koopa.color("Green")
+  
+  if abs(turtleX)> x_range or abs(turtleY) > y_range:
+    is_in_screen = False
+
+wn.bgcolor("black")
+wn.exitonclick()
+
