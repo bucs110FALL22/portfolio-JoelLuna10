15383dc3f6bc29f76a4cc4ff31f4265f22f16ef6
Joel Luna <112986791+JoelLuna10@users.noreply.github.com>
2022-10-20 00:38:53 +0000

    incomplete

---
+++ ch05/exercises/scope.py
@@ -0,0 +1,26 @@
+
+
+multnum = 0
+multnum2 = 0
+num = 0
+
+def multiply(multnum, multnum2):
+  result = 0
+  for i in range(multnum):
+    result = (multnum + multnum)
+  return result
+
+def square(num):
+  result = 0
+  for i in range(num):
+    result = result + num
+  return result
+
+
+def main():
+  m = multiply(5,4)
+  s = square(5)
+  print(m)
+  print(s)
+
+main()
