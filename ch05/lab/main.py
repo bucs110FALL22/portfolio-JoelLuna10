0cd70788ec35f557f4350873145ce2297c1396c5
Joel Luna <112986791+JoelLuna10@users.noreply.github.com>
2022-12-10 04:58:39 +0000

    complete ch8

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,25 +1,27 @@
 import pygame
 import math
 
-
+count = 0
+display = pygame.display.set_mode()
 #PartA:
 n = int(input("Enter a number: "))
 upper_limit = 20
 iters = {}
 
-def sequence(n):
-  count = 0
+def sequence(n, count, iters):
   while n != 1.0:
     count += 1
     if n%2 == 0.0 and n != 1.0:
       n = (n/2)
       print(n)
-    if n%2 == 1.0 and n != 1.0:
+    elif n%2 == 1.0 and n != 1.0:
       n = (n*3.0) + 1.0
       print(n)
     elif n == 1.0:
       print("This took", count, "attempts.")
       pygame.quit()
+    n = iters
+    iters.items()
 
 sequence(n)
 
--- ch08/lab/StringUtility.py
+++ ch08/lab/StringUtility.py
@@ -1,6 +1,53 @@
+maxVowels = 5
+
 class StringUtility():
   def __init__(self, string):
-    pass
+    self.string = string
+    
   def __str__(self):
-    pass
+    return self.string
+
+  def vowels(self):
+    self.count = 0
+    for i in self.string:
+      if i in "aeiouAEIOU":
+        self.count += 1
+    if self.count >= maxVowels:
+      return "many"
+
+  def bothEnds(self):
+    if len(self.string) > 2:
+      return self.string[0:2] + self.string[-2]
+    else:
+      return ""
+    
+  def fixStart(self):
+    self.string[0] = ""
+    if len(self.string) <= 1:
+      return self.string
+    else:
+      return "" + self.string[1:].replace("","*")
+
+  def asciiSum(self):
+    self.sum = 0
+    for i in self.string:
+      self.sum += ord(i)
+    return self.sum
+
+  def cipher(self):
+    self.encodedString =  ""
+    for i in self.string:
+      if i.isalpha() and i.isupper():
+        if ord(i) + len(self.string) < 122:
+          self.encodedString += chr(ord(words) + len(self.string) -26)
+      if i.isalpha() and i.islower():
+        if ord(i) + len(self.string) < 122:
+          self.encodedString += chr(ord(i) + len(self.string) -26)
+      else: 
+        self.encodedString +=i
+
+    return self.encodedString
+      
+    
+  
     
