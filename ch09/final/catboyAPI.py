64c4dd15fea90c0aad36cdd1f7f9e4ff94415d20
Joel Luna <112986791+JoelLuna10@users.noreply.github.com>
2022-12-12 21:12:21 +0000

    Final Exam

--- ch09/final/catboyAPI.py
+++ ch09/final/catboyAPI.py
@@ -1,8 +1,7 @@
 import requests
-#cat api, uses this to get the cat answers
+#catboy api, uses this to get random generated text from catboy
 class catboyAPI:
   def __init__(self):
-    #self.url = f'http://dog-api.kinduff.com/api/facts?number={number}'
     self.url = f'https://api.catboys.com/catboy'
 
   def get(self):
--- ch09/final/main.py
+++ ch09/final/main.py
@@ -3,12 +3,15 @@ import catboyAPI
 
 def main():
   #Proxy Class
+    #Ask user to type cat or catboy
     AskUser = str(input("Type cat to learn random facts about cats or type catboy to see what catboy will say: "))
+    #if cat is type the programm will ask the user to type any number, which chooses a fact based on the number picked
     if AskUser == 'cat':
       number = input("Type in any number to learn any random facts about cats: ")
       capi = catAPI.catAPI(number)
       results = capi.get()
       print(results)
+    #if catboy is type the programm will print out a random generated text from catboy
     elif AskUser == 'catboy':
       catbapi = catboyAPI.catboyAPI()
       results = catbapi.get()
